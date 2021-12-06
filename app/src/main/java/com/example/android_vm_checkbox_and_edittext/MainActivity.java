9474715badcfa6e936c07fdcd3e0ac47a1b48f92
olegasip <olegasip.ua@gmail.com>
2021-12-06 13:49:30 +0200

    report

--- app/src/main/java/com/example/android_vm_checkbox_and_edittext/MainActivity.java
+++ app/src/main/java/com/example/android_vm_checkbox_and_edittext/MainActivity.java
@@ -4,11 +4,92 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
 
+import android.widget.CheckBox;
+
+import android.widget.EditText;
+
+import android.widget.ImageView;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private String getName(){
+        EditText editText = (EditText) findViewById((R.id.name));
+        String name = editText.getText().toString();
+        return name;
+    }
+    public boolean onCheckOne(View view){
+        CheckBox check = findViewById(R.id.checkBoxOne);
+        boolean isCheck = check.isChecked();
+        return isCheck;
+    }
+    public boolean onCheckTwo(View view){
+        CheckBox check = findViewById(R.id.checkBoxTwo);
+        boolean isCheck2 = check.isChecked();
+        return isCheck2;
+    }
+    public boolean onCheckThree (View view){
+        CheckBox check = findViewById(R.id.checkBoxThree);
+        boolean isCheck3 = check.isChecked();
+        return isCheck3;
+    }
+    public boolean onCheckFour (View view){
+        CheckBox check = findViewById(R.id.checkBoxFour);
+        boolean isCheck4 = check.isChecked();
+        return isCheck4;
+    }
+    public boolean onCheckFive (View view){
+        CheckBox check = findViewById(R.id.checkBoxFive);
+        boolean isCheck5 = check.isChecked();
+        return isCheck5;
+    }
+    public boolean onCheckSix (View view){
+        CheckBox check = findViewById(R.id.checkBoxSix);
+        boolean isCheck6 = check.isChecked();
+        return isCheck6;
+    }
+    private String inCheckBox (View view){
+        String result;
+        if(onCheckOne(view)){
+            result="еко";
+        }
+        else if (onCheckTwo(view)){
+            result="цигляний";
+        }
+        else{
+            result="газобетонний";
+        }
+        return result;
+    }
+    private String inCheckBoxTwo (View view){
+        String result;
+        if(onCheckFour(view)){
+            result="Рококо";
+        }
+        else if (onCheckFive(view)){
+            result="Модернізм";
+        }
+        else{
+            result="Хай-тек";
+        }
+        return result;
+    }
+    public void house(View view) {
+        ImageView beforehouseImageView = (ImageView)
+                findViewById(R.id.android_house_image_view);
+        beforehouseImageView.setImageResource(R.drawable.after);
+        TextView statusInitialTextView = (TextView)
+                findViewById(R.id.status_text_view);
+        statusInitialTextView.setText(getName() +" Ваш будинок готовий.");
+        TextView statusInitialTextViewTwo = (TextView)
+                findViewById(R.id.status_text_viewTwo);
+        statusInitialTextViewTwo.setText("Це " + inCheckBox(view) + " будинок. У стилі " + inCheckBoxTwo(view));
     }
 }
